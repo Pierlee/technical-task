45432795709204442dbd62b4dc2675df8d46f7f4
Pierlee <pierleefranklin@gmail.com>
2023-08-28 10:53:14 -0300

    I deleted the dynamic project folder and moved package.json to my root folder. I added the details on how to use the website in my readme folder

---
+++ README.md
@@ -0,0 +1,51 @@
+# Web Developer Senior Assessment Task
+
+## Task Details
+
+This is a responsive webpage created as part of the Web Developer Senior Assessment Task. The task involves building a webpage using HTML, advanced CSS, and JavaScript exclusively. The webpage includes sections for navigation, hero content, about me, contact form, and dynamically generated project information using Liquid templating.
+
+### Task 1 - Advanced Web Development
+
+#### Part A: Build a Responsive Webpage
+
+1. The navigation bar at the top of the page contains links to Home, About, and Contact sections. The logo is displayed to the left.
+
+2. The hero section displays a prominent heading, subheading, and a background image.
+
+3. The About Me section provides a brief description of myself, along with a "Read More" button to expand the description using JavaScript.
+
+4. The Contact section includes a form with fields for Name, Email, and Message.
+
+#### Part B: Dynamic Content Generation
+
+1. The projects section is dynamically generated using Liquid templating. Project information is read from a JSON file and displayed with the project name, truncated description, and a link to the GitHub repository.
+
+### Usage
+
+1. Clone this repository to your local machine using Git.
+
+2. Open the `index.html` file in your web browser to access the webpage.
+
+3. Navigation: Use the navigation bar to navigate to different sections of the webpage.
+
+4. About Me: Click the "Read More..." button in the About section to expand the description.
+
+5. Contact: Fill out the form in the Contact section with your name, email, and message, then click the "Send Message" button.
+
+6. Projects: The projects section is dynamically generated based on the data in the `projects.json` file. Each project displays its name, truncated description, and a link to the GitHub repository.
+
+### Technologies Used
+
+- HTML
+- Advanced CSS (Flexbox, Grid)
+- JavaScript
+- Liquid Templating Engine
+
+### Credits
+
+- Images used in the webpage are provided in the `images` folder.
+- Liquid templating is powered by the Liquid.js library.
+
+### Author
+
+[Pierlee Guimaraes]
--- index.js
+++ index.js
@@ -6,3 +6,88 @@ document.addEventListener("DOMContentLoaded", function(){
     navLinks.classList.toggle("active")
   })
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+  const expandButton = document.getElementById("expand-button")
+  const collapsedParagraph = document.querySelector(".about-content .collapsed")
+
+  expandButton.addEventListener("click", function () {
+    if (collapsedParagraph.style.display === "none") {
+      collapsedParagraph.style.display = "block"
+      expandButton.textContent = "Read Less"
+    } else {
+      collapsedParagraph.style.display = "none"
+      expandButton.textContent = "Read More..."
+    }
+  })
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  const contactForm = document.getElementById("contact-form")
+
+  contactForm.addEventListener("submit", function (event) {
+    const nameInput = document.getElementById("name")
+    const emailInput = document.getElementById("email")
+    const messageInput = document.getElementById("message")
+
+    if (nameInput.value.trim() === "" || emailInput.value.trim() === "" || messageInput.value.trim() === "") {
+      event.preventDefault();
+      alert("Please fill out all fields.")
+    }
+  })
+})
+
+// This is where the Liquid.js code starts
+const projectsData = [
+    {
+      "name": "Grocery cart",
+      "description": "Using html, css and js exclusively, I created a grocery cart where I can write down the name of the grocery items and the quantity I want to buy. I used setItem and getItem to add the items into the local storage so if you refresh the page all of the items that you have already created are still there.",
+      "image": "images/grocery-cart.png",
+      "repository": "https://github.com/Pierlee/Grocery-Cart"
+    },
+    {
+      "name": "Organo",
+      "description": "I used react to create a webapp using ReactJS where I can add team members and the different areas they work in.",
+      "image": "images/organo.png",
+      "repository": "https://github.com/Pierlee/Organo"
+    },
+    {
+      "name": "SunnySide",
+      "description": "Using html, css and js exclusively, I created a website using the assets from frontend mentor. I used CSS and HTML and a lot of FlexBox to create a responsive website.",
+      "image": "images/sunnyside.png",
+      "repository": "https://github.com/Pierlee/SunnySide"
+    },
+    {
+      "name": "Results Summary Component",
+      "description": "I used react to create a component that would show the scores of each of your abilities and create an average for all scores",
+      "image": "images/summary-component.png",
+      "repository": "https://github.com/Pierlee/results-summary-component"
+    },
+    {
+      "name": "Calculadora",
+      "description": "Using html, css and js exclusively, I created a cool responsive caclulator that simulates a tablet and a cellphone",
+      "image": "images/calculadora.png",
+      "repository": "https://github.com/Pierlee/Calculadora"
+    }
+];
+
+const engine = new Liquid();
+
+const template = `
+  <ul>
+    {% for project in projects %}
+    <li class="project">
+      <h3>{{ project.name }}</h3>
+      <p>{{ project.description | truncatewords: 20 }}</p>
+      <a href="{{ project.repository }}" target="_blank">GitHub Repository</a>
+      <img src="{{ project.image }}" alt="{{ project.name }} Image">
+    </li>
+    {% endfor %}
+  </ul>
+`;
+
+const renderData = { projects: projectsData };
+
+engine.parseAndRender(template, renderData).then(html => {
+  document.getElementById('projects-list').innerHTML = html;
+});
